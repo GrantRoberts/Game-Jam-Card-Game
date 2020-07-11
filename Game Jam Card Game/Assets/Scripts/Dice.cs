e1a37061bb0e065713ad0afef6018db57069638e
Grant <s191075@students.aie.edu.au>
2020-07-11 12:48:13 +1000

    Can move dice with mouse, need to apply to card

---
+++ Game Jam Card Game/Assets/Scripts/Dice.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dice : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    private int m_DiceRollerIndex = 0;
+
+    private Camera m_MainCamera = null;
+
+    private Ray m_MousePosRay = new Ray();
+
+    private RaycastHit m_RayHitInfo = new RaycastHit();
+
+    private void Awake()
+    {
+        m_MainCamera = Camera.main;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        m_MousePosRay = m_MainCamera.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(m_MousePosRay, out m_RayHitInfo, float.MaxValue);
+        transform.position = m_RayHitInfo.point;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+}
