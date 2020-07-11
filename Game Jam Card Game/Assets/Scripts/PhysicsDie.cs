363e3ae39930df8ee46c2b4e5f152563cdbc8c22
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-07-11 21:11:18 +1000

    Physics dice implemented

---
+++ Game Jam Card Game/Assets/Scripts/PhysicsDie.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class PhysicsDie : MonoBehaviour
+{
+    public int value;
+
+    public Transform[] faces;
+
+    public int rotationForce = 5000;
+
+    Rigidbody r;
+
+    public TextMeshProUGUI displayOutput;
+
+    void Awake()
+    {
+        r = GetComponent<Rigidbody>();
+    }
+
+    [ContextMenu("Roll Die")]
+    public void RollDie() => RollDie(r);
+
+    public void RollDie(Rigidbody rb)
+    {
+        rb.AddTorque(new Vector3(Random.Range(-rotationForce, rotationForce), Random.Range(-rotationForce, rotationForce), Random.Range(-rotationForce, rotationForce)));
+        rb.AddForce(Vector3.up * Random.Range(400, 500));
+    }
+
+    private void Update()
+    {
+        value = int.Parse(faces.Aggregate((face1, face2) => face1.position.y > face2.position.y ? face1 : face2).name);
+        if (displayOutput)
+            displayOutput.text = value.ToString();
+    }
+}
