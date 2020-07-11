16b959c9755b4bebf9e939b492d7fbb5721dc6f3
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-07-11 16:22:12 +1000

    Text mesh pro, day/night cycles

---
+++ Game Jam Card Game/Assets/Scripts/Star.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    public DayNight dayNight;
+
+    public MeshRenderer referenceStar;
+
+    Color c;
+
+    private void Awake()
+    {
+        c = referenceStar.sharedMaterial.color;
+        print(referenceStar.sharedMaterial);
+    }
+
+    private void Update()
+    {
+        referenceStar.sharedMaterial.SetColor("_EmissionColor", c * Mathf.Clamp((dayNight.progress - 0.4f) * 25, 0, 2.5f));
+    }
+}
