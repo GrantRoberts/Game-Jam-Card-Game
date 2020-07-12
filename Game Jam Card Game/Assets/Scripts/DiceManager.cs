6d7228f2a0bcc68815905ff448fc3a319961cd43
Grant <s191075@students.aie.edu.au>
2020-07-12 15:38:26 +1000

    Dice spawn at a spawn point when rolled

--- Game Jam Card Game/Assets/Scripts/DiceManager.cs
+++ Game Jam Card Game/Assets/Scripts/DiceManager.cs
@@ -5,19 +5,43 @@ using UnityEngine.UI;
 
 public class DiceManager : MonoBehaviour
 {
-    private PhysicsDie[] m_PhysicsDice;
+    private PhysicsDie[] m_PhysicsDice = null;
+
+    public Transform m_DiceSpawnPoint = null;
+
+    private float m_SpawnPointExtentsX = 0.0f;
+
+    private float m_SpawnPointExtentsZ = 0.0f;
 
     private void Awake()
     {
         // Get all the physics dice in the scene.
         m_PhysicsDice = FindObjectsOfType<PhysicsDie>();
+
+        m_SpawnPointExtentsX = transform.position.x + (transform.localScale.x / 2);
+        m_SpawnPointExtentsZ = transform.position.z + (transform.localScale.z / 2);
+    }
+
+    private void Start()
+    {
+        RollDice();
     }
 
     public void RollDice()
     {
         for (int i = 0; i < m_PhysicsDice.Length; ++i)
         {
-            m_PhysicsDice[i].RollDie();
+            PhysicsDie currentDie = m_PhysicsDice[i];
+
+            // Spawn dice in random area in the spawn point.
+            currentDie.transform.position = new Vector3
+            (
+                Random.Range(-m_SpawnPointExtentsX, m_SpawnPointExtentsX),
+                0,
+                Random.Range(-m_SpawnPointExtentsZ, m_SpawnPointExtentsZ)
+            ) + m_DiceSpawnPoint.position;
+
+            currentDie.RollDie();
         }
     }
 }
