366bc35b164f1d9eacd7fc377f60bf0859a6816e
Grant <s191075@students.aie.edu.au>
2020-07-12 14:51:40 +1000

    Physics dice are rolled by Dice manager

--- Game Jam Card Game/Assets/Scripts/DiceManager.cs
+++ Game Jam Card Game/Assets/Scripts/DiceManager.cs
@@ -5,60 +5,19 @@ using UnityEngine.UI;
 
 public class DiceManager : MonoBehaviour
 {
-    public GameObject m_DiceObject = null;
-
-    public int m_AmountOfDice = 0;
-    
-    private int[] m_DiceRolls = null;
-
-    private List<Dice> m_DiceObjects = new List<Dice>();
-    private List<Text> m_DiceText = new List<Text>();
-
-    public List<Transform> m_DiceSpawnPoints = new List<Transform>();
-
-    private PlayerManager m_PM = null;
+    private PhysicsDie[] m_PhysicsDice;
 
     private void Awake()
     {
-        m_DiceRolls = new int[m_AmountOfDice];
-
-        // Create the dice for the manager to roll.
-        for (int i = 0; i < m_AmountOfDice; ++i)
-        {
-            m_DiceObjects.Add(Instantiate(m_DiceObject).GetComponent<Dice>());
-        }
-        m_PM = GameObject.FindObjectOfType<PlayerManager>();
+        // Get all the physics dice in the scene.
+        m_PhysicsDice = FindObjectsOfType<PhysicsDie>();
     }
 
     public void RollDice()
     {
-        // Roll dice.
-        for (int i = 0; i < m_PM.GetDiceToRoll(); ++i)
+        for (int i = 0; i < m_PhysicsDice.Length; ++i)
         {
-            m_DiceRolls[i] = Random.Range(1, 10);
-            m_DiceObjects[i].gameObject.SetActive(true);
-            m_DiceObjects[i].gameObject.transform.position = m_DiceSpawnPoints[i].position;
-            m_DiceText[i].text = m_DiceRolls[i].ToString();
+            m_PhysicsDice[i].RollDie();
         }
-
-        // Apply modifiers to dice rolls.
-        int[] rollIncreases = m_PM.GetDiceScoreIncreases();
-        int[] rollDecreases = m_PM.GetDiceScoreDecreases();
-        int diceRollsCount = m_DiceRolls.Length;
-
-        // Apply modifiers.
-        for (int i = 0; i < rollIncreases.Length; ++i)
-        {
-            m_DiceRolls[Random.Range(0, diceRollsCount - 1)] += rollIncreases[i];
-        }
-        for (int i = 0; i < rollDecreases.Length; ++i)
-        {
-            m_DiceRolls[Random.Range(0, diceRollsCount - 1)] -= rollDecreases[i];
-        }
-    }
-
-    public int GetDiceRoll(int index)
-    {
-        return m_DiceRolls[index];
     }
 }
