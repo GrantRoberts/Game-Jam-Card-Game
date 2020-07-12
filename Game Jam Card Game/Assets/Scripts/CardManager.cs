b6d1941e09fecbeee9ad1b08d67ff206b279476c
Grant <s191075@students.aie.edu.au>
2020-07-12 14:51:14 +1000

    Card manager will get card data from CardDataContainers

--- Game Jam Card Game/Assets/Scripts/CardManager.cs
+++ Game Jam Card Game/Assets/Scripts/CardManager.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
+    public CardDataContainer[] m_CardsInDeck = null;
+
     public JamesCard[] m_CardsInPlay = null;
+
+    private DiceManager m_DM = null;
+
+    private void Awake()
+    {
+        m_DM = FindObjectOfType<DiceManager>();
+        DrawCards();
+    }
+
+    public void DrawCards()
+    {
+        for (int i = 0; i < m_CardsInPlay.Length; ++i)
+        {
+            m_CardsInPlay[i].SetCardData(m_CardsInDeck[Random.Range(0, m_CardsInDeck.Length)]);
+        }
+
+        m_DM.RollDice();
+    }
 
     public void ApplyCardEffects()
     {
@@ -12,5 +32,7 @@ public class CardManager : MonoBehaviour
         {
             m_CardsInPlay[i].CheckResult();
         }
+
+        DrawCards();
     }
 }
